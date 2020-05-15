96e8c09a29deb84d813d298da8cbbeb04ccfea9f
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-15 19:36:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ get '/goodbye/:name' do
   "Goodbye #{@user_name}."
 end 
 
-get "/multiply/:num1/:num2" do 
+get '/multiply/:num1/:num2' do 
   @number1 = params[:num1]
   @number2 = params[:num2]
   "@number1 x @number2"
